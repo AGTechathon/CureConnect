1afbc8d7a3a8d2e615628f4cb1972bdb2ad41d45
Orthodox-64 <sachinprogramming62@gmail.com>
2025-06-15 04:41:43 +0530

    feat: add image symptom analysis frontend

--- web/Frontend/src/App.jsx
+++ web/Frontend/src/App.jsx
@@ -15,6 +15,18 @@ import VideoCall from "./components/VideoCall.jsx";
 import AnalysisBot from "./pages/AnalysisBot.jsx";
 import GeneralAnalysis from './pages/GeneralAnalysis.jsx';
 import SpecificAnalysis from "./pages/SpecificAnalysis.jsx";
+import AnalysisBotECG from "./pages/AnalysisBotECG.jsx";
+import ECGVideoAnalysis from './pages/ECGVideoAnalysis';
+import AnalysisBotXRAY from "./pages/AnalysisBotXRAY.jsx";
+import XRayVideoAnalysis from './pages/XRayVideoAnalysis';
+import MedicalVisionAI from "./pages/FinalCancer.jsx"
+import CancerVideoAnalysis from "./pages/CancerVideoAnalysis.jsx";
+import AlzheimerVisionAI from "./pages/Alziemer.jsx"
+import AlzheimerVideoAnalysis from "./pages/AlzheimerVideoAnalysis.jsx"
+import SkinVisionAI from "./pages/SkinAnalysis.jsx"
+import SkinVideoAnalysis from "./pages/SkinVideoAnalysis.jsx";
+import RetinolVisionAI from "./pages/Retinopathy.jsx"
+import RetinopathyVideoAnalysis from './pages/RetinopathyVideoAnalysis';
 
 
 function App() {
@@ -37,6 +49,18 @@ function App() {
           <Route path='/analysis' element={<AnalysisBot />}></Route>
           <Route path='/analysis/general' element={<GeneralAnalysis />}></Route>
           <Route path='/analysis/specific' element={<SpecificAnalysis />}></Route>
+          <Route path='/analysis/ecg' element={<AnalysisBotECG />}></Route>
+          <Route path='/analysis/ecg-video' element={<ECGVideoAnalysis />}></Route>
+          <Route path='/analysis/xray' element={<AnalysisBotXRAY />}></Route>
+          <Route path='/analysis/xray-video' element={<XRayVideoAnalysis />}></Route>
+          <Route path='/analysis/cancer' element={<MedicalVisionAI />}></Route>
+          <Route path='/analysis/cancer-video' element={<CancerVideoAnalysis />}></Route>
+          <Route path='/analysis/alzheimer' element={<AlzheimerVisionAI />}></Route>
+          <Route path='/analysis/alzheimer-video' element={<AlzheimerVideoAnalysis />}></Route>
+          <Route path='/analysis/skin' element={<SkinVisionAI />}></Route>
+          <Route path='/analysis/skin-video' element={<SkinVideoAnalysis />}></Route>
+          <Route path='/analysis/retinopathy' element={<RetinolVisionAI />}></Route> 
+          <Route path='/analysis/retinopathy-video' element={<RetinopathyVideoAnalysis />} />
           <Route
             path='/telemedicine'
             element={<VideoCall />}
