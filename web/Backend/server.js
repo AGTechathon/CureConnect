abec77e6cd83a1365a8f488a9c2433ae0abd2224
gururaj1512 <gururajgurram1512@gmail.com>
2025-06-14 17:24:30 +0530

    fix: Update CORS allowedHeaders to include 'x-forwarded-proto'

--- web/Backend/server.js
+++ web/Backend/server.js
@@ -14,7 +14,7 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-forwarded-proto'],
     exposedHeaders: ['set-cookie'],
     optionsSuccessStatus: 200
 }));
